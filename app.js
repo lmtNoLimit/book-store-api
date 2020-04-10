6e9f5239f7a1b8882160fb3b4d0fcde6e2cce418
Lam Thanh <lmt151099@gmail.com>
2020-04-10 17:17:31 +0700

    remove index in public, update routes

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/api", indexRouter);
+app.use("/", indexRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/books", auth, bookRouter);
 app.use("/api/profile", auth, usersRouter);
--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,10 @@ router.get("/", (req, res) => {
   res.render("index");
 });
 
+router.get("/api", (req, res) => {
+  res.json({
+    message: "Welcome to book store API",
+  });
+});
+
 module.exports = router;
