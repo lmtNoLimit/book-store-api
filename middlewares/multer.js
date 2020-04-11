bfa1403e70823d8d5b19a212b6dc6e1d5eafbf34
Lam Thanh <lmt151099@gmail.com>
2020-04-11 18:46:27 +0700

    add assets

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -3,7 +3,7 @@ const multer = require("multer");
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
-      cb(null, "./uploads");
+      cb(null, "./assets");
     } else {
       cb({ message: "this file is neither a video or image file" }, false);
     }
